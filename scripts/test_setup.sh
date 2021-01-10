0bbb78855de1815c8b3f163f50aba59806a818a8
Jay Vana <jaysvana@gmail.com>
2021-01-10 11:02:55 -0800

    Finish list unread and mark read

--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -7,13 +7,15 @@ DATABASE_FILE=${DATABASE_URL#"sqlite://"}
 
 if [[ -f "$DATABASE_FILE" ]]; then
   if [[ "$1" = "--destroy-data" ]]; then
+    echo "Database $DATABASE_FILE exists. Destroying it because --destroy-data was passed."
     rm $DATABASE_FILE
-    sqlx database create
-    sqlx migrate run
   else
     echo "Database $DATABASE_FILE exists. If you're sure you want to delete it, re-run with --destroy-data."
     exit 1
   fi
 fi
+
+sqlx database create
+sqlx migrate run
 
 sqlite3 "$DATABASE_FILE" < scripts/test.sql
